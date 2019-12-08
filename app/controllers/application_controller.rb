4c9dab8db3369de9a51158b50cf23d88be4f925a
aswin <aswin.ryuk.s@gmail.com>
2019-12-08 22:42:05 +0530

    Added authorization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+ include ActionController::HttpAuthentication::Basic::ControllerMethods
+ include ActionController::HttpAuthentication::Token::ControllerMethods
+ #http_basic_authenticate_with name: "admin", password: "test"
+ #before_action :restrict_access
+
+private
+
+def restrict_access
+  authenticate_or_request_with_http_token do |token, options|
+    ApiKey.where(access_token: token).exists?
+  end
+end
+
 end
